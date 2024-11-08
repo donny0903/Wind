27478181ee95a2c1aa85f44d3edacfd71c17248f
Donny <67085994+donghwan2020@users.noreply.github.com>
2024-11-09 04:39:20 +0900

    refactor: Add appLoader instance

--- Wind/WindApp.swift
+++ Wind/WindApp.swift
@@ -3,13 +3,16 @@ import SwiftUI
 @main
 struct WindApp: App {
     @AppStorage("permissionsGranted") private var permissionsGranted: Bool = false
+    @State private var showSplash = true // SplashView 제어
 
     var body: some Scene {
         WindowGroup {
-            if permissionsGranted {
-                HomeView() // 권한이 이미 부여된 경우 HomeView로 이동
+            if showSplash {
+                SplashView(showSplash: $showSplash, appLoader: AppLoader()) // appLoader 인스턴스 추가
+            } else if permissionsGranted {
+                HomeView() // 권한이 부여된 경우 HomeView로 이동
             } else {
-                PermissionView() // 권한 요청 뷰
+                PermissionView() // 권한이 없는 경우 PermissionView로 이동
             }
         }
     }
