88449540b9e6a539b6e7b50a51c46c6096b6590b
Donny <67085994+donghwan2020@users.noreply.github.com>
2024-11-09 04:46:21 +0900

    fix: Bottom sheet status and binding

--- Wind/AddGroupView.swift
+++ Wind/AddGroupView.swift
@@ -1,30 +1,29 @@
 import SwiftUI
 
 struct AddGroupView: View {
-    @Binding var isPresented: Bool // 시트 상태를 제어하기 위한 바인딩 변수
-    @State private var groupName: String = "" // 그룹 이름
-    var addGroup: (String) -> Void // 새로운 그룹을 추가하는 클로저
-
+    @Binding var isPresented: Bool
+    @Binding var groups: [String]
+    
+    @State private var groupName: String = ""
+    
     var body: some View {
         VStack {
             Text("그룹 추가")
                 .font(.headline)
                 .padding()
-
+            
             TextField("그룹 이름을 입력하세요", text: $groupName)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
-
+            
             Button("저장") {
                 if !groupName.isEmpty {
-                    addGroup(groupName) // 그룹 추가 함수 호출
-                    isPresented = false // 시트 닫기
+                    groups.append(groupName) // 그룹 추가
+                    isPresented = false // 뷰 닫기
                 }
             }
             .padding()
             .disabled(groupName.isEmpty) // 입력이 없을 경우 버튼 비활성화
-
-            Spacer()
         }
         .padding()
     }
